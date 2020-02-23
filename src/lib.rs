5fcaae4ab5d943f30e83821710541f5a5eaf4a6f
Grigoriy Baranov <baranov@w3m.co>
2020-02-23 17:27:40 +0300

    add IP_V4_ADDR RE

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
+
+lazy_static! {
+    pub static ref IP_V4_ADDR: Regex = Regex::new(
+        r"^(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)$"
+    )
+    .unwrap();
 }
